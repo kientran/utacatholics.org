c13d10cd2528f73d7b454c43a5d2ecf76f99caca
Kien Tran <kientran@kientran.com>
2010-08-02 21:22:31 -0500

    Add 404 handler to loop

--- loop.php
+++ loop.php
@@ -6,7 +6,7 @@
     <section class="post-summary">
       <?php the_excerpt(); ?>
     </section>
- <?php else : ?>
+  <?php else : ?>
     <section class="post-content">
       <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyten' ) ); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
@@ -15,3 +15,14 @@
 
 </article>
 <?php endwhile; // End of main loop. ?>
+
+<?php if (is_404() ): ?>
+<article id="post-error404">
+  <header><h1>Page not found</h1></header>
+  <section class="post error404">
+    <p><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'twentyten' ); ?></p>
+    <?php get_search_form(); ?>
+  </section>
+</article>
+<?php endif; ?>
+
