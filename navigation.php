7f625cbd7c51fe7815a9029d40d5eb509a5e94fc
Kien Tran <kientran@kientran.com>
2010-08-02 21:05:47 -0500

    Factor out navgation into a template part

---
+++ navigation.php
@@ -0,0 +1,12 @@
+<?php
+  $blogurl = get_bloginfo( 'url' );
+?>
+<ul>
+      <li class="nav home"><a href="<?php echo $blogurl ?>" class="active"><span>HOME</span><br />Return to the start</a></li>
+      <li class="nav about"><a href="<?php echo $blogurl ?>/about"><span>ABOUT</span><br />Learn who we are</a></li>
+      <li class="nav events"><a href="<?php echo $blogurl ?>/events"><span>EVENTS</span><br />Join our activities</a></li>
+      <li class="nav media"><a href="<?php echo $blogurl ?>/media"><span>MEDIA</span><br />See our pictures</a></li>
+      <li class="nav links"><a href="<?php echo $blogurl ?>/links"><span>LINKS</span><br />Visit our firends</a></li>
+      <li class="nav contact"><a href="<?php echo $blogurl ?>/contact"><span>CONTACT</span><br />Meet us in person</a></li>
+</ul>
+
